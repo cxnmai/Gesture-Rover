caa6f97a8b364c1d21c4dbff77b1a4903840635a
Chinmay Khaladkar <cbkhalad@gmail.com>
2026-02-11 10:40:03 -0800

    stuff lol

--- esp32/wifi_connect.ino
+++ esp32/wifi_connect.ino
@@ -1,10 +1,26 @@
 #include <WiFi.h>
 
-const char* ssid = "Chinmay iPhone;
-const char* password = "password321";
+#if __has_include("secrets.h")
+#include "secrets.h"
+#endif
+
+#ifndef WIFI_SSID
+#define WIFI_SSID "YOUR_SSID"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+#endif
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
 WiFiServer server(12345);
 
-const int LED_PIN = 4;
+#if defined(LED_BUILTIN)
+const int LED_PIN = LED_BUILTIN;
+#else
+const int LED_PIN = 2;
+#endif
 
 void setup() {
   Serial.begin(115200);
@@ -13,26 +29,24 @@ void setup() {
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
-  
+
   server.begin();
   Serial.println("\nCommand Link Ready: " + WiFi.localIP().toString());
 }
 
 void loop() {
+  static unsigned long last_toggle_ms = 0;
+  static bool led_on = false;
+  unsigned long now = millis();
+
+  if (now - last_toggle_ms >= 500) {
+    led_on = !led_on;
+    digitalWrite(LED_PIN, led_on ? HIGH : LOW);
+    last_toggle_ms = now;
+  }
+
   WiFiClient client = server.available();
-  if (client) {
-    while (client.connected()) {
-      if (client.available()) {
-        char val = client.read();
-        int fingers = val - '0'; // Convert char digit to int
-        
-        if (fingers > 0) {
-          digitalWrite(LED_PIN, HIGH);
-        } else {
-          digitalWrite(LED_PIN, LOW);
-        }
-        Serial.printf("Fingers detected: %d\n", fingers);
-      }
-    }
+  if (client && client.connected() && client.available()) {
+    (void)client.read();
   }
 }
